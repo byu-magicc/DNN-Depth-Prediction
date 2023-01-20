1163402defce58d1177284bfa303a73e537c0c0b
James Adams <bmcc1745@gmail.com>
2023-01-20 11:52:31 -0700

    Increased the number of features tracked and normalized

--- scripts/displacement_tracker_node.py
+++ scripts/displacement_tracker_node.py
@@ -12,6 +12,23 @@ class DisplacementTrackerNode:
 
     trackedFeaturesPath = None
 
+    # This part calibrates the pixels according to AirSim's camera characteristics
+    # Remove if using normalized pixels from the message (see below)
+    f = 465.6
+    camera_intrinsics = np.array([[f, 0, 320],
+                                [0, f, 240],
+                                [0, 0, 1]])
+
+    cam_inv = np.linalg.inv(camera_intrinsics)
+
+    def calibrate_pixels(self, pos):
+        new_pos = np.reshape(pos, (2, -1))
+        uncalibrated = np.ones((3,1))
+        uncalibrated[0:2] = new_pos
+
+        calibrated = self.cam_inv @ uncalibrated
+        return np.reshape(calibrated[0:2], pos.shape)
+    # END part
 
     def __init__(self) -> None:
         self.trackedIDs = set()
@@ -35,7 +52,7 @@ class DisplacementTrackerNode:
             
             path = self.trackedFeaturesPath[currentID]
 
-            path.append(currentFeature.pts[-1])
+            path.append(currentFeature.pts[-1]) #change to norm_pts if you want to use normalized points from tracker
             while len(path) > 2:
                 path.popleft()
             
@@ -59,14 +76,22 @@ class DisplacementTrackerNode:
             if len(path) < 2:
                 continue
 
-            disx = path[-1].x - path[-2].x
-            disy = path[-1].y - path[-2].y
+            current_feat_location = np.array([path[-1].x, path[-1].y])
+            previous_feat_location = np.array([path[-2].x, path[-2].y])
+            calibrated_current_location = self.calibrate_pixels(current_feat_location)
+            calibrated_previous_location = self.calibrate_pixels(previous_feat_location)
+            calibrated_displacement = calibrated_current_location - calibrated_previous_location
+            # disx = path[-1].x - path[-2].x
+            # disy = path[-1].y - path[-2].y
+
+            # Normalize the points according to AirSim's camera characteristics
 
             feat = FeatAndDisplacement()
             feat.feat_id=featID
-            feat.pt = path[-1]
-            feat.displacement.x = disx
-            feat.displacement.y = disy
+            feat.pt.x = calibrated_current_location[0]
+            feat.pt.y = calibrated_current_location[1]
+            feat.displacement.x = calibrated_displacement[0]
+            feat.displacement.y = calibrated_displacement[1]
             feat.displacement.z = 0
             features.append(feat)
         
--- scripts/train_neural_net_all_naive_data_relu.py
+++ scripts/train_neural_net_all_naive_data_relu.py
@@ -92,7 +92,7 @@ plot_loss(history)
 test_predictions = dnn_model.predict(test_features).flatten()
 
 a = plt.axes(aspect='equal')
-plt.scatter(test_labels, test_predictions, 0.1)
+plt.scatter(test_labels, test_predictions, 0.01)
 plt.xlabel('True Values [m]')
 plt.ylabel('Predictions [m]')
 lims = [0, 100]
